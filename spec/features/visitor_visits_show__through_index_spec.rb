d80ce95c1c057f83c9b1e48254e511c282a292a3
Luis Garcia <garcia@yahoo.com>
2018-03-09 20:03:42 -0700

    Add before function to test and test total distance

--- spec/features/visitor_visits_show__through_index_spec.rb
+++ spec/features/visitor_visits_show__through_index_spec.rb
@@ -2,28 +2,40 @@ require 'rails_helper'
 
 describe "As a visitor" do
   describe "when I visit the index page and click on a link to a trip" do
+    before(:each) do
+      @trip_1 = Trip.create(name: "Bday", start_date: "2018-02-12", end_date: "2108-02-21")
+      @trip_2 = Trip.create(name: "Spring Break", start_date: "2018-04-05", end_date: "2018-04-12")
+      @trip_3 = Trip.create(name: "Winter", start_date: "2018-12-20", end_date: "2018-12-29")
+
+      @trip_2.trails.create(name: "OBT", length: 10, address: "123 Orange Blossom Trail Rd" )
+      @trip_2.trails.create(name: "JY", length: 15, address: "111 John Young pkwy" )
+      @trip_2.trails.create(name: "SOIANINO", length: 20, address: "987 Hiawasse Rd" )
+    end
+
     it "takes me to a trip show page with the trip info" do
-      trip_1 = Trip.create(name: "Bday", start_date: "2018-02-12", end_date: "2108-02-21")
-      trip_2 = Trip.create(name: "Spring Break", start_date: "2018-04-05", end_date: "2018-04-12")
-      trip_3 = Trip.create(name: "Winter", start_date: "2018-12-20", end_date: "2018-12-29")
-
-      trip_2.trails.create(name: "OBT", length: 10, address: "123 Orange Blossom Trail Rd" )
-      trip_2.trails.create(name: "JY", length: 15, address: "111 John Young pkwy" )
-      trip_2.trails.create(name: "SOIANINO", length: 20, address: "987 Hiawasse Rd" )
-
       visit trips_path
       
       expect(page).to have_content("Bday")
       expect(page).to have_content("Spring Break")
       expect(page).to have_content("Winter")
-    
+
       click_on "Spring Break"
 
       expect(page).to have_content("Spring Break")
       expect(page).to have_content("OBT")
       expect(page).to have_content("JY")
       expect(page).to have_content("987 Hiawasse Rd")
-
     end
+
+    it "Displays total hiking distance" do
+      visit trip_path(@trip_2)
+
+      expect(page).to have_content("Spring Break")     
+      expect(page).to have_content("Total Hiking Distance: 45")     
+      expect(page).to have_content(45)     
+    end
+
+
+
   end
 end
