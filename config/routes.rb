c59b25c64273bcd4889069ca92acab9b612502c3
Luis Garcia <garcia@yahoo.com>
2018-03-09 20:03:55 -0700

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "trips#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :trips, only: [:index, :show]
   resources :trails, only: [:show]
